3679abde448d2f061e61323942cd91e78113d90a
Ashwinireddy31 <ashu.adulla@gmail.com>
2020-01-30 18:22:03 +0530

    added first project

---
+++ Firstproject/src/com/icici/loans/carloans/Rbi.java
@@ -0,0 +1,11 @@
+package com.icici.loans.carloans;
+
+public interface Rbi 
+{
+	public void withdrawl();
+	public void deposit();
+	public static void main(String[] args) {
+		Rbi i = new Rbi();
+	}
+
+}
